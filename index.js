7aea5b8556ed53181653caec8fd91ca99dd8e8a3
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-16 19:54:06 +0700

    refact, add sticky navbar to page.html

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-window.addEventListener('scroll', myFunction)
-
 const navbar = document.getElementById('navbar')
-
 const sticky = navbar.offsetTop
 
-function myFunction() {
-    if (window.scrollY >= sticky) {
+// Listen for scroll on the window
+window.addEventListener('scroll', stickyNavbar)
+
+function stickyNavbar() {
+    if (window.scrollY >= sticky) { // If the scroll gets longer then the top of the navbar, add 'sticky' class to element
         navbar.classList.add('sticky')
-    } else {
+    } else { // if not, then remove 'sticky' class
         navbar.classList.remove('sticky')
     }
 }
